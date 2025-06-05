a6d4b42bb8c1b1d9a000a204f43118764de71194
apichat <apichatlasuk13@gmail.com>
2025-06-05 15:44:11 +0700

    cm

--- project_php/env.js
+++ project_php/env.js
@@ -1,5 +1,6 @@
 // env.js
 const ENV = {
   API_URL:
-    "https://aad1-2403-6200-88a4-b7dd-d4f6-16e6-b959-f7c7.ngrok-free.app",
+    // "https://aad1-2403-6200-88a4-b7dd-d4f6-16e6-b959-f7c7.ngrok-free.app",
+    "http://localhost",
 };
